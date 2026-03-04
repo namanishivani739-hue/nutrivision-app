683b2792394de3f893184641b34a790be324b91a
namanishivani739-hue <namanishivani739@gmail.com>
2026-03-04 17:42:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,13 @@ import streamlit as st
 import joblib
 import numpy as np
 
-# Load ML model
 model = joblib.load("nutrition_model.pkl")
 
-st.title("AI-Based Child Nutrition Prediction App")
-st.write("Enter the child details below to predict nutrition status.")
-
-age = st.number_input("Age (months)", 0, 60)
-weight = st.number_input("Weight (kg)", 1.0, 30.0)
-height = st.number_input("Height (cm)", 30.0, 150.0)
+age = st.number_input("Age (months)")
+weight = st.number_input("Weight (kg)")
+height = st.number_input("Height (cm)")
 
 if st.button("Predict"):
-
     input_data = np.array([[age, weight, height]])
     prediction = model.predict(input_data)
-
-    # ML prediction + severe check
-    if weight < 5:
-        result = "Severe Malnutrition"
-    elif weight < 10:
-        result = "Under Nutrition"
-    else:
-        result = "Normal Nutrition"
-
-    st.success(f"Prediction: {result}")
+    st.success(f"Prediction: {prediction[0]}")
