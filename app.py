7131c9126abfbb54cec3f23ac29a78fb07d54c59
namanishivani739-hue <namanishivani739@gmail.com>
2026-03-04 17:30:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import numpy as np
 model = joblib.load("nutrition_model.pkl")
 
 st.title("AI-Based Child Nutrition Prediction App")
-
 st.write("Enter the child details below to predict nutrition status.")
 
 age = st.number_input("Age (months)", 0, 60)
@@ -14,8 +13,16 @@ weight = st.number_input("Weight (kg)", 1.0, 30.0)
 height = st.number_input("Height (cm)", 30.0, 150.0)
 
 if st.button("Predict"):
-    
+
     input_data = np.array([[age, weight, height]])
     prediction = model.predict(input_data)
 
-    st.success(f"Prediction: {prediction[0]}")
+    # ML prediction + severe check
+    if weight < 5:
+        result = "Severe Malnutrition"
+    elif weight < 10:
+        result = "Under Nutrition"
+    else:
+        result = "Normal Nutrition"
+
+    st.success(f"Prediction: {result}")
